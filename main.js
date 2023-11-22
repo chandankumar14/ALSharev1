c3693b1c9ec1122e97d5d05cd099ce51bc7892dc
ck@acedev <chandankumar75707@gmail.com>
2023-11-22 11:10:15 +0530

    modification in user profile image

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 require("./src/global_function")
 const knex = require("knex");
+const path = require("path")
 const express = require("express");
 const body_parser = require("body-parser");
 const swaggerJSDoc = require("swagger-jsdoc")
 const swaggerUi = require("swagger-ui-express")
 const knexConfig = require("./db/knex");
 const cors = require("cors");
-const { Model} = require('objection');
+const { Model } = require('objection');
 const v1 = require("./src/routes/index");
 const PORT_NO = process.env.PORT_NO;
 const API_URL = process.env.API_URL;
 let HOST_NO = process.env.HOST;
 // ********Knex Initializing here **********
 const knexInstance = knex(knexConfig['development']);
- Model.knex(knexInstance)
+Model.knex(knexInstance)
 //*******creating express APP here ********** */
 const App = express();
 App.use(cors());
 App.use(body_parser.json());
-App.use(body_parser.urlencoded({extended:true}))
-App.use(`/${API_URL}/`,express.static("user_profile_image"))
+App.use(body_parser.urlencoded({ extended: true }))
+App.use(`/${API_URL}/`, express.static(path.join(__dirname, "user_profile_image")))
 //*********creating Routing configuration here ********* */
 App.use(`/${API_URL}/`, v1);
 //*******************Swagger UI configuration is here*********** */

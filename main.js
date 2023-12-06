fb58c983810a3b8df729f3fb4fb1ce221d188603
ck@acedev <chandankumar75707@gmail.com>
2023-12-06 16:07:19 +0530

    development branch is merged

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const PORT_NO = process.env.DB_STAGING_PORT;
 const API_URL = process.env.API_URL;
 let HOST_NO = process.env.HOST;
 // ********Knex Initializing here **********
-const knexInstance = knex(knexConfig['development']);
+const knexInstance = knex(knexConfig['staging']);
 Model.knex(knexInstance)
 //*******creating express APP here ********** */
 const App = express();

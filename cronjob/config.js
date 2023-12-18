4886c2b392a8dcac21e1bdb28d37af3e1cf8da89
ck@acedev <chandankumar75707@gmail.com>
2023-12-18 17:22:24 +0530

    modification in join free event api

--- cronjob/config.js
+++ cronjob/config.js
@@ -1,12 +1,12 @@
 const {expireEventList} = require("../src/repository/events/event_repository")
 
-const expireEventDetails = {
-    frequency: '45 01 * * *',
-    handler: expireEventList,
-    message: "Expire event list fetch Successfully"
-}
+// const expireEventDetails = {
+//     frequency: '45 01 * * *',
+//     handler: expireEventList,
+//     message: "Expire event list fetch Successfully"
+// }
 
-module.exports = {
-    expireEventDetails,
+// module.exports = {
+//     expireEventDetails,
    
-}
+// }
